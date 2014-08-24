88fc0438e9052fe0214a2137d6f52d0f63c596fc
Robert St. John <stjohnr@bit-sys.com>
2014-08-24 14:57:37 -0600

    Initial Commit

---
+++ PersonObject/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  PersonObject
+//
+//  Created by Robert St. John on 8/24/14.
+//  Copyright (c) 2014 BIT Systems. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
